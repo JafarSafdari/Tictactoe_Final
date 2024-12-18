908b30a3453d816185f2ece1551858665aab1300
Jafar Safdari <jafar.safdari@winlas.se>
2024-12-18 09:28:34 +0100

    feat: red Should_SwitchTurn_AfterEachMove test

---
+++ Tictactoe_Final/Game.cs
@@ -0,0 +1,11 @@
+﻿namespace Tictactoe_Final;
+
+public class Game
+{
+    public char CurrentTurn { get; set; }
+
+    public void MakeMove(int i, int i1)
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ Tictactoe_Final_Tests/GameTests.cs
@@ -0,0 +1,16 @@
+﻿using Tictactoe_Final;
+
+namespace Tictactoe_Final_Tests;
+
+public class GameTests
+{
+    [Fact]
+    public void Should_SwitchTurn_AfterEachMove()
+    {
+        var game = new Game();  
+        char currentTurn = game.CurrentTurn;  
+
+        game.MakeMove(0, 0);  
+        Assert.NotEqual(currentTurn, game.CurrentTurn);  
+    }
+}
